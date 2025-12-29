a965e79397799153a2a0f95513f73104331a8a13
hyndaviponnuru63-droid <hyndaviponnuru63@gmail.com>
2025-12-29 22:42:28 +0530

    Update auth.py

--- auth.py
+++ auth.py
@@ -1,26 +1,18 @@
-import hashlib
 from db import get_connection
-
-def hash_password(password):
-    return hashlib.sha256(password.encode()).hexdigest()
 
 def signup(username, password):
     conn = get_connection()
     cur = conn.cursor()
-    cur.execute(
-        "INSERT INTO users (username, password) VALUES (%s, %s)",
-        (username, hash_password(password))
-    )
+    cur.execute("INSERT INTO users (username, password) VALUES (%s, %s)", (username, password))
     conn.commit()
     conn.close()
 
 def login(username, password):
     conn = get_connection()
     cur = conn.cursor()
-    cur.execute(
-        "SELECT id FROM users WHERE username=%s AND password=%s",
-        (username, hash_password(password))
-    )
+    cur.execute("SELECT id FROM users WHERE username=%s AND password=%s", (username, password))
     user = cur.fetchone()
     conn.close()
     return user
+
+
