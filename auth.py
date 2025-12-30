29c4d16ed277946e7481c75e7c43240883ca1975
hyndaviponnuru63-droid <hyndaviponnuru63@gmail.com>
2025-12-30 20:50:51 +0530

    Update auth.py

--- auth.py
+++ auth.py
@@ -3,16 +3,24 @@ from db import get_connection
 def signup(username, password):
     conn = get_connection()
     cur = conn.cursor()
-    cur.execute("INSERT INTO users (username, password) VALUES (%s, %s)", (username, password))
+    cur.execute(
+        "INSERT INTO users (username, password) VALUES (%s, %s)",
+        (username, password)
+    )
     conn.commit()
     conn.close()
 
 def login(username, password):
     conn = get_connection()
     cur = conn.cursor()
-    cur.execute("SELECT id FROM users WHERE username=%s AND password=%s", (username, password))
+    cur.execute(
+        "SELECT id FROM users WHERE username=%s AND password=%s",
+        (username, password)
+    )
     user = cur.fetchone()
     conn.close()
     return user
+
+
 
 
