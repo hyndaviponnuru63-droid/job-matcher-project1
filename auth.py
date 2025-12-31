0b652b0068292abbb8fc838cbbcdd95906a32d08
hyndaviponnuru63-droid <hyndaviponnuru63@gmail.com>
2025-12-31 20:30:48 +0530

    Update auth.py

--- auth.py
+++ auth.py
@@ -1,13 +1,32 @@
 from db import get_connection
+import psycopg2
+
 def signup(username, password):
-    conn = get_connection()
-    cur = conn.cursor()
-    cur.execute(
-        "INSERT INTO users (username, password) VALUES (%s, %s)",
-        (username, password)
-    )
-    conn.commit()
-    conn.close()
+    conn = None
+    try:
+        conn = get_connection()
+        cur = conn.cursor()
+        cur.execute(
+            "INSERT INTO users (username, password) VALUES (%s, %s)",
+            (username, password)
+        )
+        conn.commit()
+        return True
+
+    except psycopg2.errors.UniqueViolation:
+        if conn:
+            conn.rollback()
+        return False
+
+    except Exception as e:
+        if conn:
+            conn.rollback()
+        raise e
+
+    finally:
+        if conn:
+            conn.close()
+
 
 def login(username, password):
     conn = get_connection()
@@ -19,6 +38,8 @@ def login(username, password):
     user = cur.fetchone()
     conn.close()
     return user
+
+
 
 
 
