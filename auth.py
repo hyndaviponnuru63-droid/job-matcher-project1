8fb0f0b9e37d71f76614aaa6931b1b7928ffe821
hyndaviponnuru63-droid <hyndaviponnuru63@gmail.com>
2026-01-01 17:28:33 +0530

    Update auth.py

--- auth.py
+++ auth.py
@@ -2,15 +2,12 @@ import psycopg2
 import hashlib
 from db import get_connection
 
-
 def hash_password(password):
     return hashlib.sha256(password.encode()).hexdigest()
-
 
 def create_users_table():
     conn = get_connection()
     cur = conn.cursor()
-
     cur.execute("""
         CREATE TABLE IF NOT EXISTS users (
             id SERIAL PRIMARY KEY,
@@ -18,18 +15,14 @@ def create_users_table():
             password TEXT NOT NULL
         );
     """)
-
     conn.commit()
     cur.close()
     conn.close()
 
-
 def signup(username, password):
     create_users_table()
-
     conn = get_connection()
     cur = conn.cursor()
-
     try:
         cur.execute(
             "INSERT INTO users (username, password) VALUES (%s, %s)",
@@ -37,35 +30,29 @@ def signup(username, password):
         )
         conn.commit()
         return True
-
     except psycopg2.IntegrityError:
         conn.rollback()
         return False
-
     finally:
         cur.close()
         conn.close()
 
-
 def login(username, password):
     create_users_table()
-
     conn = get_connection()
     cur = conn.cursor()
-
     cur.execute(
         "SELECT id FROM users WHERE username=%s AND password=%s",
         (username.strip(), hash_password(password))
     )
-
     user = cur.fetchone()
-
     cur.close()
     conn.close()
-
     if user:
         return user[0]
     return None
+
+
 
 
 
